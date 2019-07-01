0512746f6280eeb737812560c9c754f3ef60e755
Nalchaz <nahel.chazot@gmail.com>
2019-07-01 18:38:23 +0200

    Display 4 best solutions for genetic

--- Genetic.py
+++ Genetic.py
@@ -37,7 +37,7 @@ def mixWithRandomSolutions(prob, pop):
 
 def generateChild(prob, pop):
 
-	rand1 = random.randint(0, len(pop.elements)*8/10)						#Element in the first 80% of the population
+	rand1 = random.randint(0, len(pop.elements)*8/10)							#Element in the first 80% of the population
 	rand2 = random.randint(len(pop.elements)*8/10+1, len(pop.elements)-1)		#Element in the last 20% of the population
 
 	parent1 = pop.elements[rand1]
@@ -46,31 +46,33 @@ def generateChild(prob, pop):
 	# print(str(parent1))
 	# print(str(parent2))
 
-	crossover = random.randint(0, len(parent1.sol))				#Determine the crossover point from where the genes will be exchanging between parents
-	firstParent = random.randint(0, 2)							#Determine if parent1 or parent2 is the first part of the child (0 if parent1, 1 if parent2)
+	crossover = random.randint(int(len(parent1.sol)*3/10), int(len(parent1.sol)*7/10))	#Determine the crossover point from where the genes will be exchanging between parents
+	firstPart = random.randint(0, 2)											#Determine if parent1 or parent2 is the first part of the child (0 if parent1, 1 if parent2)
 	child = Solution()
 
-	if firstParent == 0:
-		for i in range(0, crossover):
+	if firstPart == 0:
+		for i in range(crossover):
 			child.sol.append(parent1.sol[i])
 	else:
-		for i in range(0, crossover):
-			child.sol.append(parent2.sol[i])
+		for i in range(crossover, len(parent1.sol)):
+			child.sol.append(parent1.sol[i])
 		
 	#Here we reached the crossover so we start exchanging genes
-	y = j = crossover
+	
 
-	if firstParent == 0:
+	if firstPart == 0:
+		y = j = crossover
 		while y < len(parent2.sol):
 			while parent2.sol[j] in child.sol:
 				j = (j+1)%len(parent2.sol)
 			child.sol.append(parent2.sol[j])
 			y = y + 1
 	else:
-		while y < len(parent1.sol):
-			while parent1.sol[j] in child.sol:
-				j = (j+1)%len(parent1.sol)
-			child.sol.append(parent1.sol[j])
+		y = j = 0
+		while y < crossover:
+			while parent2.sol[j] in child.sol:
+				j = (j+1)%len(parent2.sol)
+			child.sol.append(parent2.sol[j])
 			y = y + 1
 
 	#CAN IMPLEMENT MUTATION
--- mainGenetic.py
+++ mainGenetic.py
@@ -13,21 +13,64 @@ from Classes.solution import Solution
 global _NB_ITE_MAX
 global _AUTONOMY
 
-_NB_ITE_MAX = 500
+_POPULATION_SIZE = 100
+_NB_ITE_MAX = 1000
 _AUTONOMY = 20000
 
+filename='ParaServidor_11_12/Ins10PerEPhantom'
 
-filename='ParaServidor_11_12/Ins20PerEPhantom'
+def displayBestSolutions(pop, prob):
+	DrawPath.subplot(2, 2, 1)
+	DrawPath.setPlotTitle("Solution 1", fontsize=19)
+
+	bat = Batteries.setBatteries(pop.elements[0].sol, _AUTONOMY, prob)
+
+	DrawPath.drawPoints(prob.xini, prob.yini, prob.x, prob.y, bat)
+	DrawPath.drawLines(prob.xini, prob.yini, prob.x, prob.y, pop.elements[0].sol)
+
+	DrawPath.subplot(2, 2, 2)
+	DrawPath.setPlotTitle("Solution 2", fontsize=19)
+
+	bat = Batteries.setBatteries(pop.elements[1].sol, _AUTONOMY, prob)
+
+	DrawPath.drawPoints(prob.xini, prob.yini, prob.x, prob.y, bat)
+	DrawPath.drawLines(prob.xini, prob.yini, prob.x, prob.y, pop.elements[1].sol)
+
+	DrawPath.subplot(2, 2, 3)
+	DrawPath.setPlotTitle("Solution 3", fontsize=19)
+
+	bat = Batteries.setBatteries(pop.elements[2].sol, _AUTONOMY, prob)
+
+	DrawPath.drawPoints(prob.xini, prob.yini, prob.x, prob.y, bat)
+	DrawPath.drawLines(prob.xini, prob.yini, prob.x, prob.y, pop.elements[2].sol)
+
+	DrawPath.subplot(2, 2, 4)
+	DrawPath.setPlotTitle("Solution 4", fontsize=19)
+
+	bat = Batteries.setBatteries(pop.elements[3].sol, _AUTONOMY, prob)
+
+	DrawPath.drawPoints(prob.xini, prob.yini, prob.x, prob.y, bat)
+	DrawPath.drawLines(prob.xini, prob.yini, prob.x, prob.y, pop.elements[3].sol)
+
+	DrawPath.draw()
+
+
+
 
 prob = Problem(filename)
-pop = Genetic.generateRandomPopulation(prob, 50)
+pop = Genetic.generateRandomPopulation(prob, _POPULATION_SIZE)
 
 print(pop)
 
 for i in range(0, _NB_ITE_MAX):
-	if i%500 == 0:
+	# child = Genetic.generateChild(prob, pop)
+	# print(str(child))
+	# print("")
+	if i%200 == 0:
 		Genetic.mixWithRandomSolutions(prob, pop)
 	childPop = Genetic.generateChildPopulation(prob, pop)
 	pop = Genetic.getNewGeneration(pop, childPop)
 
 print(pop)
+
+displayBestSolutions(pop, prob)
